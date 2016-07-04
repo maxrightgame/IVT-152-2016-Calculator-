3e556f7bebb7471d81cfdcbe2c98a04a699798f4
maxrightgame <maxrightgame@gmail.com>
2016-07-04 12:59:16 +0600

    1.2

--- Calc Attempt Two/Calc Attempt Two/Form1.cs
+++ Calc Attempt Two/Calc Attempt Two/Form1.cs
@@ -19,22 +19,30 @@ namespace Calc_Attempt_Two
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            double result = Convert.ToDouble(textBox1.Text)*Convert.ToDouble(textBox2.Text);
+            textBox3.Text = result.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            double result = Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text);
+            textBox3.Text = result.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            double result = Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text);
+            textBox3.Text = result.ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+           if (Convert.ToInt32(textBox2.Text) != 0)
+            {
+                double result = Convert.ToDouble(textBox1.Text)/Convert.ToDouble(textBox2.Text);
+                textBox3.Text = result.ToString();
+            }
+            else textBox3.Text = "0";
         }
     }
 }
