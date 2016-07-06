ed70a15068292459784f3e20c3c924a51f93a057
qwerwer <polina.didenko@mail.ru>
2016-07-06 12:57:22 +0600

    1.15 Completed all factory tests

---
+++ Calc Attempt Two/Calc Attempt Two.Tests/Functions with two operators/TwoOperatorsFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calc_Attempt_Two.Functions_with_two_operators;
+using NUnit.Framework;
+
+namespace Calc_Attempt_Two.Tests.Functions_with_two_operators
+{
+    [TestFixture]
+    public class TwoOperatorsFactoryTests
+    {
+        [TestCase("button1", typeof(Addition))]
+        [TestCase("button2", typeof(Subtraction))]
+        [TestCase("button3", typeof(Multiplication))]
+        [TestCase("button4", typeof(Division))]
+        [TestCase("button8", typeof(XToGrade))]
+        [TestCase("button14", typeof(sqrt_x_y))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoOperatorsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
