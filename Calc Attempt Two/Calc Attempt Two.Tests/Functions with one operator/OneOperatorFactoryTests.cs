6104de0b0f406a645df4805b27cdd1ce2d1250da
qwerwer <polina.didenko@mail.ru>
2016-07-06 12:52:06 +0600

    1.14 Added unit tests for Types OneOperatorFactoryTests

---
+++ Calc Attempt Two/Calc Attempt Two.Tests/Functions with one operator/OneOperatorFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Calc_Attempt_Two.Functions_with_one_operator;
+using NUnit.Framework;
+
+namespace Calc_Attempt_Two.Tests.Functions_with_two_operators
+{
+    [TestFixture]
+    public class OneOperatorFactoryTests
+    {
+        [TestCase("button15", typeof(_10to_grade))]
+        [TestCase("button10", typeof(Abs))]
+        [TestCase("button12", typeof(cos))]
+        [TestCase("button9", typeof(Ln))]
+        [TestCase("button11", typeof(sin))]
+        [TestCase("button5", typeof(Sqrt))]
+        [TestCase("button7", typeof(Log10))]
+        [TestCase("button6", typeof(Squaring))]
+        [TestCase("button13", typeof(tan))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneOperatorFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
